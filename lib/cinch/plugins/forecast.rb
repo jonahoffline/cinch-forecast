d8b0fa5eb342a2e7e3def7463a69756559f74bd4
Jonah Ruiz <jonah@pixelhipsters.com>
2013-06-07 02:52:51 -0400

    Refactor and fix exception issue.

--- lib/cinch/plugins/forecast.rb
+++ lib/cinch/plugins/forecast.rb
@@ -8,107 +8,97 @@ module Cinch
     class Forecast
       include Cinch::Plugin
 
-      API_KEY  = ENV['WUNDERGROUND_KEY']
-      BASE_URL = "http://api.wunderground.com/api/#{API_KEY}/"
-
-
       match /forecast (.+)/
-
-      attr_accessor :location, :conditions
-
       def execute(m, query)
-        @location, @conditions = nil, {}
-
-        geolookup(query)
-        get_conditions
+        ##
+        # Gets location using zipcode
+        location = geolookup(query)
+        return m.reply "No results found for #{query}." if location.nil?
 
         ##
-        # Reply with the Weather Summary
-        m.reply(weather_summary)
+        # Gets the weather conditions
+        data = get_conditions(location)
+        return m.reply 'Problem getting data. Try again later.' if data.nil?
+
+        ##
+        # Returns the weather summary
+        m.reply(weather_summary(data))
       end
 
       def geolookup(zipcode)
-        results = open("#{BASE_URL}geolookup/q/#{zipcode}.json").read
-        data = JSON.parse(results)
-        @location = data['location']['l']
+        location = JSON.parse(open("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/geolookup/q/#{zipcode}.json").read)
+        location['location']['l']
       rescue
-        "No results found for #{zipcode}"
+        nil
       end
 
-      def get_conditions
-        results = open("#{BASE_URL}conditions#{@location}.json").read
+      def get_conditions(location)
+        data = JSON.parse(open("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/conditions#{location}.json").read)
 
         ##
-        # Cleanup and store into Hash
-        clean_up(JSON.parse(results))
+        # Clean up and return as an OpenStruct
+        OpenStruct.new(
+            ##
+            # County, State & Country
+            county: data['current_observation']['display_location']['full'],
+            country: data['current_observation']['display_location']['country'],
+
+            ##
+            # Latitude and Longitude
+            lat: data['current_observation']['display_location']['latitude'],
+            lng: data['current_observation']['display_location']['longitude'],
+
+            ##
+            # Observation Time and General Weather Information
+            observation_time: data['current_observation']['observation_time'],
+            weather: data['current_observation']['weather'],
+            temp_fahrenheit: data['current_observation']['temp_f'],
+            temp_celcius: data['current_observation']['temp_c'],
+            relative_humidity: data['current_observation']['relative_humidity'],
+            feels_like: data['current_observation']['feelslike_string'],
+            uv: data['current_observation']['UV'],
+
+            ##
+            # Wind Related Data
+            wind: data['current_observation']['wind_string'],
+            wind_direction: data['current_observation']['wind_dir'],
+            wind_degrees: data['current_observation']['wind_degrees'],
+            wind_mph: data['current_observation']['wind_mph'],
+            wind_gust_mph: data['current_observation']['wind_gust_mph'],
+            wind_kph: data['current_observation']['wind_kph'],
+            pressure_mb: data['current_observation']['pressure_mb'],
+
+            ##
+            # Forecast URL
+            forecast_url: data['current_observation']['forecast_url']
+        )
       rescue
-        'No weather data was found.'
+        nil
       end
 
-      def clean_up(data)
+      def weather_summary(data)
         ##
-        # County, State & Country
-        @conditions.store(:county, data['current_observation']['display_location']['full'])
-        @conditions.store(:country, data['current_observation']['display_location']['country'])
-
-        ##
-        # Latitude and Longitude
-        @conditions.store(:lat, data['current_observation']['display_location']['latitude'])
-        @conditions.store(:lng, data['current_observation']['display_location']['longitude'])
-
-        ##
-        # Observation Time
-        @conditions.store(:observation_time, data['current_observation']['observation_time'])
-
-        ##
-        # General Weather Information
-        @conditions.store(:weather, data['current_observation']['weather'])
-        @conditions.store(:temp_fahrenheit, data['current_observation']['temp_f'])
-        @conditions.store(:temp_celcius, data['current_observation']['temp_c'])
-        @conditions.store(:relative_humidity, data['current_observation']['relative_humidity'])
-        @conditions.store(:feels_like, data['current_observation']['feelslike_string'])
-        @conditions.store(:uv, data['current_observation']['UV'])
-
-        ##
-        # Wind Related Data
-        @conditions.store(:wind, data['current_observation']['wind_string'])
-        @conditions.store(:wind_direction, data['current_observation']['wind_dir'])
-        @conditions.store(:wind_degrees, data['current_observation']['wind_degrees'])
-        @conditions.store(:wind_mph, data['current_observation']['wind_mph'])
-        @conditions.store(:wind_gust_mph, data['current_observation']['wind_gust_mph'])
-        @conditions.store(:wind_kph, data['current_observation']['wind_kph'])
-        @conditions.store(:pressure_mb, data['current_observation']['pressure_mb'])
-
-        ##
-        # Forecast URL
-        @conditions.store(:forecast_url, data['current_observation']['forecast_url'])
-        @conditions.store(:fetched_from, "#{BASE_URL}conditions#{@location}.json")
-
-        @conditions = OpenStruct.new(@conditions)
-      rescue
-        'Problem getting data.'
-      end
-
-      def weather_summary
-        ##
-        # Sample Summary using geolookup('00687')
-        # Weather for: Morovis, PR, US
-        # Party Cloudy, feels like 85 F (27.1 C)
+        # Sample Summary using !forecast 00687
+        # Forecast for: Morovis, PR, US
+        # Latitude: 18.32682228, Longitude: -66.40519714
+        # Weather is Partly Cloudy, feels like 85 F (27.1 C)
         # UV: 9.5, Humidity: 78%
         # Wind: From the SE at 1.0 MPH Gusting to 5.0 MPH
         # Direction: East, Degrees: 90
         # Last Updated on June 4, 11:25 PM AST
         # More Info: http://www.wunderground.com/US/PR/Morovis.html
 
-        %Q{Weather for: #{@conditions.county}, #{@conditions.country}
-        #{@conditions.weather}, #{@conditions.feels_like}
-        UV: #{@conditions.uv}, Humidity: #{@conditions.relative_humidity}
-        Wind: #{@conditions.wind}
-        Direction: #{@conditions.wind_direction}, Degrees: #{@conditions.wind_degrees},
-        #{@conditions.observation_time}
-        More Info: #{@conditions.forecast_url}}
+        %Q{
+          Forecast for: #{data.county}, #{data.country}
+          Latitude: #{data.lat}, Longitude: #{data.lng}
+          Weather is #{data.weather}, #{data.feels_like}
+          UV: #{data.uv}, Humidity: #{data.relative_humidity}
+          Wind: #{data.wind}
+          Direction: #{data.wind_direction}, Degrees: #{data.wind_degrees},
+          #{data.observation_time}
+          More Info: #{data.forecast_url}}
       rescue
-        'Problem fetching the weather summary.'
+        'Problem fetching the weather summary. Try again later.'
       end
     end
   end
